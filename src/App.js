1d3a55187658b10ea7e078954ce5b46af76cb355
Millene Ishiyama <milleneeishiyama@gmail.com>
2022-11-30 16:02:31 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TopImage from "./assets/top-image.svg";
+import BottomImage from "./assets/bottom-image.svg";
+import Rocket from "./assets/rocket.svg";
+import Subscribe from "./components/Button";
+import "./components/Timer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img className="img-top" src={TopImage} />
+      <section>
+        <div className="div-left">
+          <h1>Ready to launch in...</h1>
+          <ul>
+            <li>Dias</li>
+            <li>Horas</li>
+            <li>Minutos</li>
+            <li>Segundos</li>
+          </ul>
+          <h2 className="timer"></h2>
+          <p>Inscreva-se para saber mais sobre o lançamento</p>
+          <button onClick={Subscribe}>Inscreva-se</button>
+        </div>
+        <div className="div-right">
+          <img src={Rocket} />
+        </div>
+      </section>
+      <img className="img-bottom" src={BottomImage} />
     </div>
   );
 }
