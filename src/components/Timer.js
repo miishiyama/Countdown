de01324b3cc763d6ba15475ec3a3b9d7a3fb8ce8
Millene Ishiyama <milleneeishiyama@gmail.com>
2023-03-15 09:43:59 -0700

    Organizing code

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -5,18 +5,26 @@ export default function startTimer(duration, display) {
         hours = parseInt(((timer / 60) / 60) % 24, 10)
         minutes = parseInt((timer / 60) % 60, 10)
         seconds = parseInt(timer % 60, 10)
-        days = days < 10 ? "0" + days : days
-        hours = hours < 10 ? "0" + hours : hours
-        minutes = minutes < 10 ? "0" + minutes : minutes
-        seconds = seconds < 10 ? "0" + seconds : seconds
+        days = days < 10 
+            ? "0" + days 
+            : days
+        hours = hours < 10 
+            ? "0" + hours 
+            : hours
+        minutes = minutes < 10 
+            ? "0" + minutes 
+            : minutes
+        seconds = seconds < 10 
+            ? "0" + seconds 
+            : seconds
         display.textContent = days + " : " + hours + " : " + minutes + " : " + seconds
-        if(--timer < 0) {
+        if (--timer < 0) {
             timer = duration
         }
     }, 1000)
 }
 
-window.onload = function() {
+window.onload = function () {
     var duration = 3600 * 24 * 10
     var display = document.querySelector(".timer")
     startTimer(duration, display)
